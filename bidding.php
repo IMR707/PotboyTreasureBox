28f4afb697bdc56dc695fe017b7d055a5109b5c3
z4qk3n0by <ruzaini@ourtech.my>
2017-05-16 11:43:59 +0800

    add new

--- bidding.php
+++ bidding.php
@@ -51,11 +51,229 @@ use Carbon\Carbon;
           ?>
 
 
+          <div class="panel panel-flat">
+            <div class="panel-body">
+
+
+              <div class="col-sm-3 col-md-3 col-xs-3 text-center">
+                  <img src="<?php echo UPLOADURL;?>a4.svg" class="imgsmall" alt="">
+                  <h2>Instant Claim</h2>
+              </div>
+              <div class="col-sm-3 col-md-3 col-xs-3 text-center">
+                  <img src="<?php echo UPLOADURL;?>a2.svg" class="imgsmall" alt="">
+                  <h2 >Coming Up Soon</h2>
+              </div>
+              <div class="col-sm-3 col-md-3 col-xs-3 text-center">
+                  <img src="<?php echo UPLOADURL;?>a4.svg" class="imgsmall" alt="">
+                  <h2 >Winner Sharing</h2>
+              </div>
+
+              <div class="col-sm-3 col-md-3 col-xs-3 text-center">
+                  <img src="<?php echo UPLOADURL;?>a2.svg" class="imgsmall" alt="">
+                  <h2 >Rules T&C</h2>
+              </div>
+            </div>
+          </div>
+
+
+
+          	<div class="panel panel-flat">
+							<div class="panel-body">
+								<div class="tabbable">
+									<ul class="nav nav-tabs nav-tabs-solid nav-tabs-component nav-justified">
+										<li class="active"><a href="#solid-rounded-justified-tab1" data-toggle="tab">Top</a></li>
+										<li><a href="#solid-rounded-justified-tab2" data-toggle="tab">Progress</a></li>
+                    <li><a href="#solid-rounded-justified-tab3" data-toggle="tab">New</a></li>
+                    <li class="dropdown">
+											<a href="#" class="dropdown-toggle" data-toggle="dropdown">Price <span class="caret"></span></a>
+											<ul class="dropdown-menu dropdown-menu-right">
+												<li><a href="#solid-rounded-justified-tab4" data-toggle="tab">Expensive first</a></li>
+												<li><a href="#solid-rounded-justified-tab5" data-toggle="tab">Cheap first</a></li>
+											</ul>
+										</li>
+									</ul>
+
+									<div class="tab-content">
+										<div class="tab-pane active" id="solid-rounded-justified-tab1">
 
 
 
 
-        
+                      <div class="col-sm-6 col-md-6 col-xs-6 text-center">
+                    				<div class="panel panel-flat timeline-content">
+          										<div class="panel-body">
+          											<a href="#" class="display-block content-group">
+          												<img src="<?php echo UPLOADURL;?>cover.jpg" class="img-responsive" alt="">
+          											</a>
+
+          											<h6 class="content-group text-left">
+          												Fitbit Surge (Small)-min 5,00 Gold</h6>
+
+                                  <div class="progress">
+                    								<div class="progress-bar bg-purple" style="width: 92.8%">
+                    									<span><i id="" class="icon-spinner4 spinner position-left"></i>92.8% Complete</span>
+                    								</div>
+                    							</div>
+          										</div>
+
+          										<div class="panel-footer panel-footer-transparent">
+          											<div class="heading-elements">
+          												<ul class="list-inline list-inline-condensed heading-text">
+          													<li><a href="#" class="text-default">Need 900</a></li>
+
+          												</ul>
+
+          												<span class="heading-btn pull-right">
+          													<a href="#" class="btn bg-purple">Join Now</a>
+          												</span>
+          											</div>
+          										</div>
+          									</div>
+
+
+                      </div>
+
+
+                      <div class="col-sm-6 col-md-6 col-xs-6 text-center">
+                    				<div class="panel panel-flat timeline-content">
+          										<div class="panel-body">
+          											<a href="#" class="display-block content-group">
+          												<img src="<?php echo UPLOADURL;?>cover.jpg" class="img-responsive" alt="">
+          											</a>
+
+          											<h6 class="content-group text-left">
+          												Fitbit Surge (Small)-min 5,00 Gold</h6>
+
+                                  <div class="progress">
+                    								<div class="progress-bar bg-purple" style="width: 85%">
+                    									<span><i id="" class="icon-spinner4 spinner position-left"></i>85% Complete</span>
+                    								</div>
+                    							</div>
+          										</div>
+
+          										<div class="panel-footer panel-footer-transparent">
+          											<div class="heading-elements">
+          												<ul class="list-inline list-inline-condensed heading-text">
+          													<li><a href="#" class="text-default">Need 900</a></li>
+          													<li><a href="#" class="text-default">Bids 800</a></li>
+          												</ul>
+
+          												<span class="heading-btn pull-right">
+          													<a href="#" class="btn bg-purple">Claim Now</a>
+          												</span>
+          											</div>
+          										</div>
+          									</div>
+
+
+                      </div>
+
+                      <div class="col-sm-6 col-md-6 col-xs-6 text-center">
+                    				<div class="panel panel-flat timeline-content">
+          										<div class="panel-body">
+          											<a href="#" class="display-block content-group">
+          												<img src="<?php echo UPLOADURL;?>cover.jpg" class="img-responsive" alt="">
+          											</a>
+
+          											<h6 class="content-group text-left">
+          												Fitbit Surge (Small)-min 5,00 Gold</h6>
+
+                                  <div class="progress">
+                    								<div class="progress-bar bg-purple" style="width: 85%">
+                    									<span><i id="" class="icon-spinner4 spinner position-left"></i>85% Complete</span>
+                    								</div>
+                    							</div>
+          										</div>
+
+          										<div class="panel-footer panel-footer-transparent">
+          											<div class="heading-elements">
+          												<ul class="list-inline list-inline-condensed heading-text">
+          													<li><a href="#" class="text-default">Need 900</a></li>
+          													<li><a href="#" class="text-default">Bids 800</a></li>
+          												</ul>
+
+          												<span class="heading-btn pull-right">
+          													<a href="#" class="btn bg-purple">Claim Now</a>
+          												</span>
+          											</div>
+          										</div>
+          									</div>
+
+
+                      </div>
+
+                      <div class="col-sm-6 col-md-6 col-xs-6 text-center">
+                    				<div class="panel panel-flat timeline-content">
+          										<div class="panel-body">
+          											<a href="#" class="display-block content-group">
+          												<img src="<?php echo UPLOADURL;?>cover.jpg" class="img-responsive" alt="">
+          											</a>
+
+          											<h6 class="content-group text-left">
+          												Fitbit Surge (Small)-min 5,00 Gold</h6>
+
+                                  <div class="progress">
+                    								<div class="progress-bar bg-purple" style="width: 85%">
+                    									<span><i id="" class="icon-spinner4 spinner position-left"></i>85% Complete</span>
+                    								</div>
+                    							</div>
+          										</div>
+
+          										<div class="panel-footer panel-footer-transparent">
+          											<div class="heading-elements">
+          												<ul class="list-inline list-inline-condensed heading-text">
+          													<li><a href="#" class="text-default">Need 900</a></li>
+          													<li><a href="#" class="text-default">Bids 800</a></li>
+          												</ul>
+
+          												<span class="heading-btn pull-right">
+          													<a href="#" class="btn bg-purple">Claim Now</a>
+          												</span>
+          											</div>
+          										</div>
+          									</div>
+
+
+                      </div>
+
+
+
+
+
+
+
+
+
+
+
+										</div>
+
+										<div class="tab-pane" id="solid-rounded-justified-tab2">
+											Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid laeggin.
+										</div>
+
+										<div class="tab-pane" id="solid-rounded-justified-tab3">
+											DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork. Williamsburg whatever.
+										</div>
+
+                    <div class="tab-pane" id="solid-rounded-justified-tab4">
+											Aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthet.
+										</div>
+                    <div class="tab-pane" id="solid-rounded-justified-tab5">
+											test123 ganic, assumenda labore aesthet. nulla singl
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+
+
+
+
+
+
+
 
 
  			</div>
--- homeSlider.php
+++ homeSlider.php
@@ -3,18 +3,12 @@ $homeSlider=$list->FEgetHomeSlider();
 ?>
 <div class="panel panel-flat">
   <div class="panel-body">
-
-
             <div class="col-sm-12 col-md-12">
               <div id="carousel-notification" class="bootstrap-carousel" data-indicators="true" data-controls="true">
-
                 <?php foreach ($homeSlider as $key => $value):?>
                 <img src="backend/uploads/<?php echo $value->img_name?>" data-title="" class="img-responsive">
                 <?php endforeach; ?>
               </div>
             </div>
-
-    <!-- <h6 class="text-semibold">Start your development with no hassle!</h6>
-    <p class="content-group">Common problem of templates is that all code is deeply integrated into the core. This limits your freedom in decreasing amount of code, i.e. it becomes pretty difficult to remove unnecessary code from the project. Limitless allows you to remove unnecessary and extra code easily just by removing the path to specific LESS file with component styling. All plugins and their options are also in separate files. Use only components you actually need!</p> -->
   </div>
 </div>
