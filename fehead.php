6802c2ca9aeca92ee481be53907be27888a66be3
z4qk3n0by <ruzaini@ourtech.my>
2017-05-18 10:40:25 +0800

    add lala

--- fehead.php
+++ fehead.php
@@ -25,6 +25,29 @@
  <script type="text/javascript" src="<?php echo FRONTJS;?>bootbox.min.js"></script>
  <script src="<?php echo BACK_PLUGIN; ?>moment.min.js" type="text/javascript"></script>
  <script type="text/javascript" src="<?php echo BACK_PAGES_SCRIPT; ?>jquery.countdown.js"></script>
+ <script type="text/javascript">
+
+   function a(url){
+     var useraccess=<?php echo $user->useraccess;?>;
+
+     switch (useraccess) {
+       case 0:
+       alert('0');
+       break;
+      case 1:
+      alert('0');
+      break;
+      case 2:
+      alert('0');
+      break;
+       default:
+
+     }
+     <?php
+pre($user);
+     ?>
+   }
+ </script>
 
  <!-- /core JS files -->
 
@@ -148,9 +171,79 @@ $crdata=$list->FEgetRewardData(($user->logged_in) ? $user->uid:0);
 
        } ?>
 
+
+         <li class=""><button type="button" class="btn bg-blue btn-block" data-toggle="modal" data-target="#modal-login"><i class="icon-comment position-left"></i> Launch modal</button></li>
+
+
+
      </ul>
    </div>
  </div>
+
+
+ <div id="modal-login" class="modal fade">
+   <div class="modal-dialog">
+     <div class="modal-content login-form">
+
+       <!-- Form -->
+       <form class="modal-body" action="index.html">
+         <div class="text-center">
+           <div class="icon-object border-slate-300 text-slate-300"><i class="icon-reading"></i></div>
+           <h5 class="content-group">Login to your account <small class="display-block">Your credentials</small></h5>
+         </div>
+
+         <div class="form-group has-feedback has-feedback-left">
+           <input type="text" class="form-control" placeholder="Username">
+           <div class="form-control-feedback">
+             <i class="icon-user text-muted"></i>
+           </div>
+         </div>
+
+         <div class="form-group has-feedback has-feedback-left">
+           <input type="text" class="form-control" placeholder="Password">
+           <div class="form-control-feedback">
+             <i class="icon-lock2 text-muted"></i>
+           </div>
+         </div>
+
+         <div class="form-group login-options">
+           <div class="row">
+             <div class="col-sm-6">
+               <label class="checkbox-inline">
+                 <input type="checkbox" class="styled" checked="checked">
+                 Remember
+               </label>
+             </div>
+
+             <div class="col-sm-6 text-right">
+               <a href="login_password_recover.html">Forgot password?</a>
+             </div>
+           </div>
+         </div>
+
+         <div class="form-group">
+           <button type="submit" class="btn bg-blue btn-block">Login <i class="icon-arrow-right14 position-right"></i></button>
+         </div>
+
+         <div class="content-divider text-muted form-group"><span>or sign in with</span></div>
+         <ul class="list-inline form-group list-inline-condensed text-center">
+           <li><a href="#" class="btn border-indigo text-indigo btn-flat btn-icon btn-rounded"><i class="icon-facebook"></i></a></li>
+           <li><a href="#" class="btn border-pink-300 text-pink-300 btn-flat btn-icon btn-rounded"><i class="icon-dribbble3"></i></a></li>
+           <li><a href="#" class="btn border-slate-600 text-slate-600 btn-flat btn-icon btn-rounded"><i class="icon-github"></i></a></li>
+           <li><a href="#" class="btn border-info text-info btn-flat btn-icon btn-rounded"><i class="icon-twitter"></i></a></li>
+         </ul>
+
+         <div class="content-divider text-muted form-group"><span>Don't have an account?</span></div>
+         <a href="login_registration.html" class="btn btn-default btn-block content-group">Sign up</a>
+         <span class="help-block text-center no-margin">By continuing, you're confirming that you've read our <a href="#">Terms &amp; Conditions</a> and <a href="#">Cookie Policy</a></span>
+       </form>
+       <!-- /form -->
+
+     </div>
+   </div>
+ </div>
+ <!-- /login form -->
+ 
  <!-- /second navbar -->
 <?php
 
