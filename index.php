f4101475b29fe86171ac0abe12c84e7322ebc358
z4qk3n0by <ruzaini@ourtech.my>
2017-05-18 14:34:25 +0800

    add lala

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ use Carbon\Carbon;
  						<!-- Horizontal form -->
  						<div class="panel panel-flat" style="margin-bottom:5px !important">
  							<div class="panel-body" style="padding:5px!important;">
- 								<a href="dailyReward.php"><img src="http://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/11/plainDoge-700x525.jpg" data-title="doge" data-content="Hey there!" class="img-responsive"></a>
+ 								<a href="dailyReward.php"><img src="https://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/11/plainDoge-700x525.jpg" data-title="doge" data-content="Hey there!" class="img-responsive"></a>
                 <a href="dailyReward.php"  class="btn btn-purple" name="button" style="display: block; width: 100%;">Next Daily <br> GOLD box in <br><b id="time">xx:xx:xx</b></a>
  							</div>
  						</div>
@@ -63,7 +63,7 @@ use Carbon\Carbon;
  						<!-- Horizontal form -->
  						<div class="panel panel-flat" style="margin-bottom:5px !important">
  							<div class="panel-body" style="padding:5px!important;">
- 								<img src="http://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/11/plainDoge-700x525.jpg" data-title="doge" data-content="Hey there!" class="img-responsive">
+ 								<img src="https://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/11/plainDoge-700x525.jpg" data-title="doge" data-content="Hey there!" class="img-responsive">
                 <button type="button" class="btn btn-purple" name="button" style="display: block; width: 100%;">Free Games <br>in <br><b id="time">xx:xx:xx</b></button>
  							</div>
  						</div>
@@ -76,7 +76,7 @@ use Carbon\Carbon;
  						<!-- Horizontal form -->
  						<div class="panel panel-flat" style="margin-bottom:5px !important">
  							<div class="panel-body" style="padding:5px!important;">
- 								<a href="goldConversion.php"><img src="http://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/11/plainDoge-700x525.jpg" data-title="doge" data-content="Hey there!" class="img-responsive"></a>
+ 								<a href="goldConversion.php"><img src="https://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/11/plainDoge-700x525.jpg" data-title="doge" data-content="Hey there!" class="img-responsive"></a>
                 <a href="goldConversion.php"  class="btn btn-purple" name="button" style="display: block; width: 100%;">Convert <br> DIAMOND to <br>GOLD</a>
  							</div>
  						</div>
@@ -86,7 +86,7 @@ use Carbon\Carbon;
  						<!-- Horizontal form -->
  						<div class="panel panel-flat" style="margin-bottom:5px !important">
  							<div class="panel-body" style="padding:5px!important;">
- 								<img src="http://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/11/plainDoge-700x525.jpg" data-title="doge" data-content="Hey there!" class="img-responsive">
+ 								<img src="https://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/11/plainDoge-700x525.jpg" data-title="doge" data-content="Hey there!" class="img-responsive">
                 <button type="button" class="btn btn-purple" name="button" style="display: block; width: 100%;">Play it <br>Now<br> <b id="time">1 Diamond</b></button>
  							</div>
  						</div>
