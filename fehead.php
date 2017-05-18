1291012c226801e6e2f3d0571a836fc06999a81f
z4qk3n0by <ruzaini@ourtech.my>
2017-05-18 14:35:46 +0800

    add lala

--- fehead.php
+++ fehead.php
@@ -11,7 +11,7 @@ $helper = $fb->getRedirectLoginHelper();
 $permissions = ['email']; // Optional permissions
 $loginUrl = $helper->getLoginUrl('https://potboy.com.my/treasurebox/fb-callback.php', $permissions);
 
-echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+//echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
 
 
 
