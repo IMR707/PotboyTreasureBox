a7c0c47fb5b3afa97d7b6fe95117a60714f5ce03
z4qk3n0by <ruzaini@ourtech.my>
2017-05-19 15:31:36 +0800

    add deek5

--- fehead.php
+++ fehead.php
@@ -306,7 +306,7 @@ $crdata=$list->FEgetRewardData(($user->logged_in) ? $user->uid:0);
        <li class="<?php echo isActived('DASH', $pagemenu, 'active')?>"><a href="index.php"><i class="icon-display4 position-left"></i> Dashboard</a></li>
        <li class="<?php echo isActived('BID', $pagemenu, 'active')?>"><a href="bidding.php"><i class="icon-display4 position-left"></i> Bidding</a></li>
        <li class=""><a href="index.php"><i class="icon-display4 position-left"></i> Latest Winners</a></li>
-       <li class=""><a href="index.php"><i class="icon-display4 position-left"></i> Wishlist Voting</a></li>
+       <li class="<?php echo isActived('BID', $pagemenu, 'active')?>"><a href="wishlist.php"><i class="icon-display4 position-left"></i> Wishlist Voting</a></li>
        <?php
        if (!$user->logged_in) {
            ?>
