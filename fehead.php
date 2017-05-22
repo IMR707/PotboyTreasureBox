be9c392b475f20080e429ffa1773060650fd55d5
mon <fazrin@ourtech.my>
2017-05-22 11:59:43 +0800

    form ui update

--- fehead.php
+++ fehead.php
@@ -428,10 +428,10 @@ $crdata=$list->FEgetRewardData(($user->logged_in) ? $user->uid:0);
           <div class="form-group">
             <label class="control-label col-md-3 col-sm-3 col-xs-3">Name</label>
             <div class="col-md-9 col-sm-9 col-xs-9">
-              <div class="col-md-6 col-sm-6 col-xs-6">
+              <div class="col-md-6 col-sm-12 col-xs-12">
                 <input type="text" class="form-control" placeholder="First Name" value="<?php echo $fn;?>" required name="firstname">
               </div>
-              <div class="col-md-6 col-sm-6 col-xs-6">
+              <div class="col-md-6 col-sm-12 col-xs-12">
                 <input type="text" class="form-control" placeholder="Last Name" value="<?php echo $ln;?>" required name="lastname">
               </div>
             </div>
