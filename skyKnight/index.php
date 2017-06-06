33a3a85c6829b8fcf09506d0407fe73bd2c3a17d
ChunLamLee <chunlam_sees@hotmail.com>
2017-06-06 14:28:00 +0800

    game update
    better setting handling

--- skyKnight/index.php
+++ skyKnight/index.php
@@ -1,5 +1,10 @@
-﻿<!DOCTYPE html>
-<html manifest="offline.appcache">
+﻿<?php 
+
+	session_start();
+
+?>
+<!DOCTYPE html>
+<html >
 <head>
     <meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
